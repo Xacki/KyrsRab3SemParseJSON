167861da16648069f5cb2ccebfa9bbde9611ef78
Raevsky Ra <kaneky.ky@outlook.com>
2022-09-13 18:58:05 +0300

    0.0.0.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "json files (*.json)|*.json";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(670, 329);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(793, 386);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Парсер JSON";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace KyrsRab3SemParseJSON
 {
@@ -15,6 +19,53 @@ namespace KyrsRab3SemParseJSON
         public Form1()
         {
             InitializeComponent();
+
+        }
+        class j
+        {
+            public int jf { get; set; }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string json = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var fileStream = openFileDialog1.OpenFile();
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    json = reader.ReadToEnd().Replace(" ","").Replace("\n", "");
+                }
+
+            }
+
+            List<string> list = new List<string>();
+            
+            for (int i = 0; i < json.Length; i++)
+            {
+                if (json[i].ToString() == "[" ||
+                    json[i].ToString() == "]" ||
+                    json[i].ToString() == "{" ||
+                    json[i].ToString() == "}")
+                {
+                    list.Add("");
+                    list[list.Count - 1] += json[i].ToString();
+                    list.Add("");
+                }
+                else
+                {
+                    list[list.Count - 1] += json[i].ToString();
+                }
+            }
+
+
+            listBox1.Items.Add(json);
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
